48ddc30002d45a5906b06cc823b3da577ff88634
Slengpack <slengpack@gmail.com>
2023-10-20 16:47:08 +0300

    Home task 3 final version 2

--- HT_03/task_01.py
+++ HT_03/task_01.py
@@ -2,9 +2,7 @@
 # can be hardcoded. The "replacement" value is entered by user. The number of elements in the tuples must be different.
 
 tuple_list = [(1, 5, True), ("aaa", 2), (0,)]
-counter = 0
-for current_tuple in tuple_list:
-    replacing_value = input(f"Input value for replacing in {counter + 1} tuple: ")
-    tuple_list[counter] = current_tuple[:len(current_tuple) - 1] + tuple(replacing_value)
-    counter += 1
+replacing_value = input(f"Input value for replacing: ")
+for i in range(len(tuple_list)):
+    tuple_list[i] = tuple_list[i][:len(tuple_list[i]) - 1] + tuple(replacing_value)
 print(f'List with replaced elements: {tuple_list}')
