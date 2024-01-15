95d408d1b2c37f31c79b17aa85cdc0a645ca7712
Slengpack <slengpack@gmail.com>
2024-01-16 00:13:16 +0200

    Home task 19 final version 2 (small fixes)

--- HT_19/products/urls.py
+++ HT_19/products/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
-from . import views
+from .views import product_input
+from .views import ProductsListView
+from .views import ProductDetailView
+from .views import cart_view
 from .views import add_to_cart
 from .views import increase_item_in_cart_quantity
 from .views import decrease_item_in_cart_quantity
@@ -10,15 +13,14 @@ from .views import submit_order
 
 app_name = "products"
 urlpatterns = [
-    path("", views.product_input, name="product_input"),
-    path("products", views.ProductsListView.as_view(), name="products_list"),
-    path("product/<int:pk>", views.ProductDetailView.as_view(), name="product_details"),
-    path("cart", views.cart_view, name="products_in_cart_list"),
-    path("add_to_cart", add_to_cart, name="add_to_cart"),
-    path("increase_item_in_cart_quantity", increase_item_in_cart_quantity, name="increase_item_in_cart_quantity"),
-    path("decrease_item_in_cart_quantity", decrease_item_in_cart_quantity, name="decrease_item_in_cart_quantity"),
-    path("remove_item_in_cart", remove_item_in_cart, name="remove_item_in_cart"),
-    path("clear_item_in_cart", clear_item_in_cart, name="clear_item_in_cart"),
-    path("clear_item_in_cart", clear_item_in_cart, name="clear_item_in_cart"),
-    path("submit_order", submit_order, name="submit_order"),
+    path("", product_input, name="product_input"),
+    path("products/", ProductsListView.as_view(), name="products_list"),
+    path("products/<int:pk>/", ProductDetailView.as_view(), name="product_details"),
+    path("cart/", cart_view, name="products_in_cart_list"),
+    path("cart/add/", add_to_cart, name="add_to_cart"),
+    path("cart/quantity/increase/", increase_item_in_cart_quantity, name="increase_item_in_cart_quantity"),
+    path("cart/quantity/decrease/", decrease_item_in_cart_quantity, name="decrease_item_in_cart_quantity"),
+    path("cart/remove/", remove_item_in_cart, name="remove_item_in_cart"),
+    path("cart/clear/", clear_item_in_cart, name="clear_item_in_cart"),
+    path("cart/submit/", submit_order, name="submit_order"),
 ]
